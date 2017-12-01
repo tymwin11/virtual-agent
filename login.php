3591ceb41fbb3effa6ddf56ec31a88971a26dd99
Sarina Phejlada <sarina1116@gmail.com>
2017-12-01 16:28:41 -0500

    Update login.php

--- login.php
+++ login.php
@@ -4,22 +4,13 @@
     // $test = "tnguyen366";
     if(isset($_POST['submit'])){
         $db = new mysqli("localhost", $test, $test, $test);
-        $query = "SELECT * FROM admin WHERE user = '" . $_POST['user'] . "' AND pass = '" . $_POST['pass'] . "'";
+        $query = "SELECT * FROM users WHERE login = '" . $_POST['user'] . "' AND password = '" . $_POST['pass'] . "'";
         $sql = $db->query($query);
         $n = $sql->num_rows;
         if($n > 0)
             header("Location: home.php");
         else
             $error = "Invalid login username or password";
-    }
-    if(isset($_POST['register'])){
-        $db = new mysqli("localhost", $test, $test, $test);
-        $sql = "INSERT INTO admin(user, pass) VALUES ('". $_POST['user'] . "','" . $_POST['pass'] . "')";
-        $rs = $db->query($sql);
-        if($rs)
-            $error = "User Successfully Created";
-        else
-            $error = "Error: User not created";
     }
 ?>
 <html>
@@ -44,7 +35,7 @@
         </style>
     </head>
     <body>
-        <h1>Login to Music Base</h1>
+        <h1>Login to [insert company name here]</h1>
         <form method = "POST">
             <div class="login">
               <label><b>Username</b></label>
@@ -54,7 +45,6 @@
               <input type="password" placeholder="Enter Password" name="pass" required><br>
           
               <button type = "submit" name = "submit">Login</button>
-              <button type = "submit" name = "register">Register</button>
               <div class = "error"><?php echo $error; ?></div>
             </div>
         </form>

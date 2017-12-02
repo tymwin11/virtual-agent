f59f519b15a3ffcf7eb2e532c8cebb2c3d2fcf81
Timothy Nguyen <tnguyen366@student.gsu.edu>
2017-12-01 19:33:49 -0500

    Update viewcart.php

--- viewcart.php
+++ viewcart.php
@@ -13,21 +13,8 @@
     </head>
     <body>
         <?php
-            $host = "localhost"; 
-            $user = "tnguyen366"; 
-            $pass = "tnguyen366"; 
-            $db = "tnguyen366";
-            $i = 0;
-            $r = mysql_connect($host, $user, $pass);
-            if (!$r) {
-                echo "Could not connect to server\n";
-                trigger_error(mysql_error(), E_USER_ERROR);
-            }
-            $r2 = mysql_select_db($db);
-            if (!$r2) {
-                echo "Cannot select database\n";
-                trigger_error(mysql_error(), E_USER_ERROR);
-            }
+            session_start();
+            include('connect.php');
             
             $query = "Select shoppingcart.customerid, shoppingcart.date_initialized, shoppingcart_items.productid, shoppingcart_items.product_quantity, shoppingcart_items.product_price FROM shoppingcart, shoppingcart_items WHERE shoppingcart.cartid = shoppingcart_items.cartid";
             $rs = mysql_query($query);

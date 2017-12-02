896ee3a325fa5806ce459c995b63cf10c271186b
Timothy Nguyen <tnguyen366@student.gsu.edu>
2017-12-02 12:08:43 -0500

    Add files via upload

--- viewcart.php
+++ viewcart.php
@@ -15,6 +15,15 @@
         <?php
             session_start();
             include('connect.php');
+            $query = "SELECT * FROM users";
+            $rs = mysql_query($query);
+            if (!$rs) {
+                echo "Could not execute query: $query";
+                trigger_error(mysql_error(), E_USER_ERROR); 
+            }
+            while ($row = mysql_fetch_assoc($rs)){
+                $_SESSION['user_first'] = $row['firstname'];
+            }
             
             $query = "Select shoppingcart.customerid, shoppingcart.date_initialized, shoppingcart_items.productid, shoppingcart_items.product_quantity, shoppingcart_items.product_price FROM shoppingcart, shoppingcart_items WHERE shoppingcart.cartid = shoppingcart_items.cartid";
             $rs = mysql_query($query);
@@ -26,9 +35,8 @@
             while ($row = mysql_fetch_assoc($rs)){
                 $cart_count[$i] = $row['cartid'];
                 $i++;
-            echo $row['customerid']." ".$row['date_initialized']." ".$row['productid']." ".$row['product_quantity']." ".$row['product_price'];
             }
-            echo "<div class='viewcart'> Cart: <a href='#shoppingcart.php'>".count($cart_count)."</a></div>";
+            echo "<div class='viewcart'>Welcome ".$_SESSION['user_first']." Cart: <a href='shoppingcart.php'>".count($cart_count)."</a></div>";
         ?>
     </body>
 </html>

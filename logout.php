49eeed42a3b27229b9e975a60e9711d4b15d57fe
Sarina Phejlada <sarina1116@gmail.com>
2017-12-03 14:16:30 -0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,17 @@
 <?php
     session_start();
+    include 'connect.php';
+    include 'header.php';
     session_unset();
+?>
+<html>
+    <head>
+        <title>Logout</title>
+    </head>
+    <body>
+        <h1>You've Logged Out</h1>
+    </body>
+</html>
+<?php
     session_destroy();
-    header('Location: home.php');
 ?>

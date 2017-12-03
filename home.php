87b5cc2a5a0f206bf3648f334401f229fd752500
Sarina Phejlada <sarina1116@gmail.com>
2017-12-03 14:26:47 -0500

    Update home.php

--- home.php
+++ home.php
@@ -7,8 +7,6 @@
     <meta charset="UTF-8"/>
     <title>Tacocat Travel Agency - Home</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="styles/styles.css"/>
-    <link rel="stylesheet" href="styles/login.css"/>
     <link rel="stylesheet" href="styles/home.css"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>

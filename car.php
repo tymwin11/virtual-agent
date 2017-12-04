778b1706745971130157cf96af275fcc7fbbe9bb
Sarina Phejlada <sarina1116@gmail.com>
2017-12-04 00:03:39 -0500

    Update car.php

--- car.php
+++ car.php
@@ -1,12 +1,12 @@
 <?php
     session_start();
     include('connect.php');
-    include('viewcart.php');
     include('header.php');
     if(isset($_POST['submit'])){
         $type = $_POST['type'];
         $customerid = $_SESSION['user_id'];
-        $sql = "SELECT productid, price, quantity FROM inventory WHERE name = '$type'";
+        $fee = $type . " Fee";
+        $sql = "SELECT productid, price, quantity FROM inventory WHERE name = '$fee'";
         $result = mysql_query($sql);
         if (mysql_num_rows($result) > 0) {
             while($row = mysql_fetch_assoc($result)) {
@@ -16,9 +16,48 @@
             }
         } 
         else {
-            echo "0 results";
+            //echo "0 results";
         }
-        $sql = "INSERT INTO shoppingcart VALUES($customerid, $productid, $product_quantity, $product_price)";
+        $sql = "INSERT INTO shoppingcart VALUES($customerid, $productid, 1, $product_price)";
+        $rs = mysql_query($sql);
+        if (!$rs) {
+            echo "Could not execute query: $sql";
+            trigger_error(mysql_error(), E_USER_ERROR); 
+        }
+        $product_quantity-=1;
+        $sql = "UPDATE inventory SET quantity = $product_quantity WHERE productid = $productid";
+        $rs = mysql_query($sql);
+        if (!$rs) {
+            echo "Could not execute query: $sql";
+            trigger_error(mysql_error(), E_USER_ERROR); 
+        }
+        //DATE
+        $date1=date_create($_POST['pickup']);
+        $date2=date_create($_POST['dropoff']);
+        $diff=date_diff($date1,$date2);
+        $days = $diff->format("%a");
+        $daily = $type . " Per Day";
+        $sql = "SELECT productid, price, quantity FROM inventory WHERE name = '$daily'";
+        $result = mysql_query($sql);
+        if (mysql_num_rows($result) > 0) {
+            while($row = mysql_fetch_assoc($result)) {
+                $productid = $row["productid"];
+                $product_price = $row["price"];
+                $product_quantity = $row["quantity"];
+            }
+        } 
+        else {
+            //echo "0 results";
+        }
+        $product_price *= $days;
+        $sql = "INSERT INTO shoppingcart VALUES($customerid, $productid, $days, $product_price)";
+        $rs = mysql_query($sql);
+        if (!$rs) {
+            echo "Could not execute query: $sql";
+            trigger_error(mysql_error(), E_USER_ERROR); 
+        }
+        $product_quantity -= $days;
+        $sql = "UPDATE inventory SET quantity = $product_quantity WHERE productid = $productid";
         $rs = mysql_query($sql);
         if (!$rs) {
             echo "Could not execute query: $sql";
@@ -52,10 +91,10 @@
             <h1>Car Rental</h1>
             <form method = "post">
                 Select Car Type<select name = "type">
-                    <option value = "SUV Fee">SUV</option>
-                    <option value = "Compact Fee">Compact</option>
-                    <option value = "Midsize Fee">Midsize</option>
-                    <option value = "Luxury Fee">Luxury</option>
+                    <option value = "SUV">SUV</option>
+                    <option value = "Compact">Compact</option>
+                    <option value = "Midsize">Midsize</option>
+                    <option value = "Luxury">Luxury</option>
                 </select><br>
                 Pickup<input type = "date" name = "pickup"><br>
                 Dropoff<input type = "date" name = "dropoff"><br>
@@ -64,10 +103,3 @@
         </div>
     </body>
 </html>
-<?php 
-$date1=date_create($_POST['pickup']);
-$date2=date_create($_POST['dropoff']);
-$diff=date_diff($date1,$date2);
-$days = $diff->format("%a");
-echo $days;
-?>

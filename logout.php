077582732a30076c1bbbc6794db1508925d38779
Sarina Phejlada <sarina1116@gmail.com>
2017-12-04 00:05:30 -0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,6 +2,12 @@
     session_start();
     include 'connect.php';
     include 'header.php';
+    $sql = "TRUNCATE table shoppingcart";
+    $rs = mysql_query($sql);
+    if (!$rs) {
+        echo "Could not execute query: $sql";
+        trigger_error(mysql_error(), E_USER_ERROR); 
+    }
     session_unset();
 ?>
 <html>

42479991103086237c6be658831ed9618de1060e
Sarina Phejlada <sarina1116@gmail.com>
2017-12-03 19:38:49 -0500

    Update home.php

--- home.php
+++ home.php
@@ -5,12 +5,8 @@
 ?>
 <html>
 <head>
-    <meta charset="UTF-8"/>
     <title>Tacocat Travel Agency - Home</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="styles/home.css"/>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
     
@@ -24,13 +20,6 @@
         <a href = "park.php">
             <img src = "src/parking.jpg">
         </a>
-    </div>
-    <div id="content">
-        <form action="home.php" action="POST">
-            <input class="input_text" name="search" placeholder="..."><input class="input_text" type="submit" name="search" value="Search"><br>
-        </form>
-        
-        
     </div>
 </body>
 <script src="scripts/scripts.js"></script>

be0739d154e1586521a015ae959dafce6cab96b1
Sarina Phejlada <sarina1116@gmail.com>
2017-12-03 22:56:35 -0500

    Update car.php

--- car.php
+++ car.php
@@ -3,26 +3,27 @@
     include('connect.php');
     include('viewcart.php');
     include('header.php');
-    $type = $_POST['type'];
-    $query = "SELECT * FROM users";
-    $rs = mysql_query($query);
-    if (!$rs) {
-        echo "Could not execute query: $query";
-        trigger_error(mysql_error(), E_USER_ERROR); 
-    }
-    while ($row = mysql_fetch_assoc($rs)){
-        $_SESSION['user'] = $row['login'];
-    }
-    $user_session = $_SESSION['user'];
     if(isset($_POST['submit'])){
-        $sql = "UPDATE shoppingcart SET car = $type where user = ".$_SESSION['user'];
+        $type = $_POST['type'];
+        $customerid = $_SESSION['user_id'];
+        $sql = "SELECT productid, price, quantity FROM inventory WHERE name = '$type'";
+        $result = mysql_query($sql);
+        if (mysql_num_rows($result) > 0) {
+            while($row = mysql_fetch_assoc($result)) {
+                $productid = $row["productid"];
+                $product_price = $row["price"];
+                $product_quantity = $row["quantity"];
+            }
+        } 
+        else {
+            echo "0 results";
+        }
+        $sql = "INSERT INTO shoppingcart VALUES($customerid, $productid, $product_quantity, $product_price)";
         $rs = mysql_query($sql);
         if (!$rs) {
-            echo "Could not execute query: $query";
+            echo "Could not execute query: $sql";
             trigger_error(mysql_error(), E_USER_ERROR); 
         }
-        $rs = mysql_query($query);
-        mysql_close();
     }
 ?>
 <html>
@@ -49,12 +50,12 @@
     <body>
         <div class = "renting">
             <h1>Car Rental</h1>
-            <form method = "POST">
+            <form method = "post">
                 Select Car Type<select name = "type">
-                    <option value = "SUV">SUV</option>
-                    <option value = "Compact">Compact</option>
-                    <option value = "Midsize">Midsize</option>
-                    <option value = "Luxury">Luxury</option>
+                    <option value = "SUV Fee">SUV</option>
+                    <option value = "Compact Fee">Compact</option>
+                    <option value = "Midsize Fee">Midsize</option>
+                    <option value = "Luxury Fee">Luxury</option>
                 </select><br>
                 Pickup<input type = "date" name = "pickup"><br>
                 Dropoff<input type = "date" name = "dropoff"><br>
@@ -63,3 +64,10 @@
         </div>
     </body>
 </html>
+<?php 
+$date1=date_create($_POST['pickup']);
+$date2=date_create($_POST['dropoff']);
+$diff=date_diff($date1,$date2);
+$days = $diff->format("%a");
+echo $days;
+?>

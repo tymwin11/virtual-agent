c7cad256948126332e8c73d8e7f17f10b63e5897
Timothy Nguyen <tnguyen366@student.gsu.edu>
2017-12-04 12:38:37 -0500

    Add files via upload

--- profile.php
+++ profile.php
@@ -42,29 +42,19 @@
             session_start();
             include('connect.php');
             include('header.php');
-            $query1 = "SELECT * FROM users";
-            $rs = mysql_query($query1);
-            if (!$rs) {
-                echo "Could not execute query: $query1";
-                trigger_error(mysql_error(), E_USER_ERROR); 
+            if($_SESSION['signin']){
+                $query2 = "SELECT * FROM orders";
+                $rs = mysql_query($query2);
+                if (!$rs) {
+                    echo "Could not execute query: $query2";
+                    trigger_error(mysql_error(), E_USER_ERROR); 
+                }
             }
-            while ($row = mysql_fetch_assoc($rs)){
-                $_SESSION['user_id'] = $row['customerid'];
-                $_SESSION['user_first'] = $row['firstname'];
-                $_SESSION['user_last'] = $row['lastname'];
-                $_SESSION['user_address'] = $row['address'];
-            }
-            $query2 = "SELECT * FROM orders";
-            $rs = mysql_query($query2);
-            if (!$rs) {
-                echo "Could not execute query: $query2";
-                trigger_error(mysql_error(), E_USER_ERROR); 
-            }
-            while ($row = mysql_fetch_assoc($rs)){
-                $_SESSION['order_id'] = $row['orderid'];
-                $_SESSION['order_status'] = $row['status'];
-                $_SESSION['date'] = $row['date_purchased'];
-            }
+                while ($row = mysql_fetch_assoc($rs)){
+                    $_SESSION['order_id'] = $row['orderid'];
+                    $_SESSION['order_status'] = $row['status'];
+                    $_SESSION['date'] = $row['date_purchased'];
+                }  
         ?> 
         <div class="panel">
             <h1 class="panelheader">Personal Information</h1>
@@ -77,9 +67,14 @@
             <h2 class="panelheader">Past Purchases</h2>
             <table>
                 <tr>
-                    <th><?PHP echo $_SESSION['order_id']?></th>
-                    <th><?PHP echo $_SESSION['order_status']?></th>
-                    <th><?PHP echo $_SESSION['date']?></th>
+                    <th>Order ID#</th>
+                    <th>Order Status</th>
+                    <th>Date</th>
+                </tr>
+                <tr>
+                    <td><?PHP echo $_SESSION['order_id']?></td>
+                    <td><?PHP echo $_SESSION['order_status']?></td>
+                    <td><?PHP echo $_SESSION['date']?></td>
                 </tr>
             </table>
         </div>

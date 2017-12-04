30615bd04f0ef881b61499848c5be59d2d83bfef
Timothy Nguyen <tnguyen366@student.gsu.edu>
2017-12-04 01:10:29 -0500

    Add files via upload

--- logout.php
+++ logout.php
@@ -1,3 +1,4 @@
+
 <?php
     session_start();
     include 'connect.php';
@@ -9,15 +10,27 @@
         trigger_error(mysql_error(), E_USER_ERROR); 
     }
     session_unset();
+    session_destroy();
 ?>
 <html>
     <head>
         <title>Logout</title>
     </head>
     <body>
-        <h1>You've Logged Out</h1>
+        <?php
+            session_start();
+            include 'connect.php';
+            include 'header.php';
+            $sql = "TRUNCATE table shoppingcart";
+            $rs = mysql_query($sql);
+            if (!$rs) {
+                echo "Could not execute query: $sql";
+                trigger_error(mysql_error(), E_USER_ERROR); 
+            }
+            session_unset();
+            session_destroy();
+            header("Location: home.php");
+        ?>
     </body>
 </html>
-<?php
-    session_destroy();
-?>
+

0d51b17fc0d4a25e362eff82c2834f2c0ca2df35
Sarina Phejlada <sarina1116@gmail.com>
2017-12-04 20:23:04 -0500

    Update car.php

--- car.php
+++ car.php
@@ -82,7 +82,16 @@
                 color: black;
             }
             h1{
+                border: 12px inset grey;
+                background-color:darkgrey;
+                margin: 10px;
                 text-align: center;
+                color:black;
+                text-shadow: 1px 1px 2px white, 0 0 25px white, 0 0 5px white;
+                padding: 20px;
+                font-size: 60px;
+                font-family: Comic;
+                font-style: bold;
             }
         </style>
     </head>
